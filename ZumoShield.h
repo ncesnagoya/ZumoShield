b7a3d4e073deae42b2c05739026fddfe78bd3bf1
exhonda <honda@ertl.jp>
2017-01-14 14:23:26 +0900

    commit

--- ZumoShield.h
+++ ZumoShield.h
@@ -11,13 +11,13 @@
 
 template <typename T> float heading(LSM303::vector<T> v, LSM303 *compass)
 {
-  float x_scaled =  2.0*(float)(v.x - compass->m_min.x) / (compass->m_max.x - compass->m_min.x) - 1.0;
-  float y_scaled =  2.0*(float)(v.y - compass->m_min.y) / (compass->m_max.y - compass->m_min.y) - 1.0;
+	float x_scaled =  2.0*(float)(v.x - compass->m_min.x) / (compass->m_max.x - compass->m_min.x) - 1.0;
+	float y_scaled =  2.0*(float)(v.y - compass->m_min.y) / (compass->m_max.y - compass->m_min.y) - 1.0;
 
-  float angle = atan2(y_scaled, x_scaled)*180 / M_PI;
-  if (angle < 0)
-    angle += 360;
-  return angle;
+	float angle = atan2(y_scaled, x_scaled)*180 / M_PI;
+	if (angle < 0)
+	  angle += 360;
+	return angle;
 }
 
 #define CRB_REG_M_2_5GAUSS 0x60 // CRB_REG_M value for magnetometer +/-2.5 gauss full scale
