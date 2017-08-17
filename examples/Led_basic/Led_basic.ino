3276da6c7cb329af1eb3fccd9af16261258053d7
exhonda <honda@ertl.jp>
2017-08-17 09:21:05 +0900

    Remove comment

--- examples/Led_basic/Led_basic.ino
+++ examples/Led_basic/Led_basic.ino
@@ -2,15 +2,12 @@
 #include <ZumoShield.h>
 
 void setup() {
- //一度だけ実行 
+
 }
 
 void loop() {
-  //LEDをONに
   led.on();
-  //1秒経過するのを待つ
   delay(1000);
-  //LEDをOFFに
   led.off();
   delay(1000);
 }
